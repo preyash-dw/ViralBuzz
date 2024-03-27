4bdb7be16e32669893f7b7791ab1b2c70a7a7e04
preyash-dw <122265433+preyash-dw@users.noreply.github.com>
2024-03-27 13:18:33 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import contentRoute from './routes/testRouting.js';
+import contentRoute from './routes/routing.js';
 import userRoute from "./routes/userRoutes.js"
 dotenv.config();
 
